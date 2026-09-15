a5d4d991c36c26e63fe4bfbc6e2bc324d057ec5a
Khansa Kamila <khansakamila467@gmail.com>
2026-09-15 15:02:55 +0700

    feat: integrate NProgress for page loading indicators and update styles

---
+++ src/components/NProgress.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ showSpinner: false });
+
+function NProgressHandler() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    NProgress.start();
+    const timer = setTimeout(() => NProgress.done(), 300);
+    return () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+  }, [pathname, searchParams]);
+
+  return null;
+}
+
+export default function NProgressProvider() {
+  return (
+    <Suspense>
+      <NProgressHandler />
+    </Suspense>
+  );
+}
